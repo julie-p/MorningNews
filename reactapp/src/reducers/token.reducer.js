e9b8bd948216a5759cd01da46c47c0cd13dbbf3c
Julie P <paupertjulie@gmail.com>
2020-05-19 17:58:01 +0200

    corrected a typo

--- reactapp/src/reducers/token.reducer.js
+++ reactapp/src/reducers/token.reducer.js
@@ -2,8 +2,7 @@ export default function (token = '', action) {
 
     switch (action.type) {
         case 'addToken':
-            let newToken = action.token;
-            return newToken;
+            return action.token;
 
         default:
             return token;
